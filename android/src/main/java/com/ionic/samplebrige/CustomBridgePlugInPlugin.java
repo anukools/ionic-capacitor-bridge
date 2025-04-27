bfe3ed3b9883b5d97a2b7727e51e5104e8762d30
Anukool Srivastav <anukool.srivastav@religare.com>
2025-04-27 12:47:08 +0530

    Add CustomBridgePlugInPlugin.java for custom Capacitor bridge

--- android/src/main/java/com/ionic/samplebrige/CustomBridgePlugInPlugin.java
+++ android/src/main/java/com/ionic/samplebrige/CustomBridgePlugInPlugin.java
@@ -1,22 +1,36 @@
 package com.ionic.samplebrige;
 
+import android.content.Intent;
+import android.util.Log;
+
 import com.getcapacitor.JSObject;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-@CapacitorPlugin(name = "CustomBridgePlugIn")
+@CapacitorPlugin(name = "MyCustomBridge")
 public class CustomBridgePlugInPlugin extends Plugin {
 
-    private CustomBridgePlugIn implementation = new CustomBridgePlugIn();
+    private static final String TAG = "CustomBridgePlugin";
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
-
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+    public void showFlutterView(PluginCall call) {
+        String message = call.getString("message");
+        if (message != null) {
+            // Send broadcast to start Flutter activity
+            Intent intent = new Intent("com.ionic.START_FLUTTER");
+            intent.putExtra("message", message);
+            getContext().sendBroadcast(intent);
+            call.resolve();
+        } else {
+            call.reject("Message is required");
+        }
     }
-}
+
+    public void sendMessageToJS(String message) {
+        JSObject ret = new JSObject();
+        ret.put("message", message);
+        notifyListeners("onFlutterMessage", ret);
+    }
+} 
